2c325963cb62a202004d71b7f2541b3ab2a3dd40
PavelGilevich <pavel-gilevich@mail.ru>
2014-06-06 14:17:20 +0000

    Final commit SYSFS lab

--- calc.c
+++ calc.c
@@ -1,16 +1,10 @@
-#define SYSFS
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/string.h>
 
-#ifdef SYSFS
 #include <linux/init.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
-#else
-#include <linux/proc_fs.h>
-#endif
 
 // names
 #define ARG1 "arg1"
@@ -22,15 +16,11 @@
 
 
 #define WRITE_SIZE 100
+
 static char arg1_input[WRITE_SIZE];
 static char arg2_input[WRITE_SIZE];
 static char operation_input[WRITE_SIZE];
 
-struct proc_dir_entry *calc_dir;
-struct proc_dir_entry *arg1;
-struct proc_dir_entry *arg2;
-struct proc_dir_entry *operation;
-struct proc_dir_entry *result;
 
 //Calculate Function
 long calculate(void)
@@ -58,7 +48,6 @@ long calculate(void)
   return res;
 }
 
-#ifdef SYSFS
 // description args
 //kernel obj attribute
 static struct attribute arg1 = {
@@ -88,6 +77,7 @@ static struct attribute * calc_attributes[] = {
   &result,
   NULL
 };
+
 
 static ssize_t default_show(struct kobject *kobj, struct attribute *attr, char *buf)
 {
@@ -144,115 +134,15 @@ static int __init sysfsexample_module_init(void)
   return err;
 }
 
-#else
-/*
-* arg1 write handler
-*/
-int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
+static void __exit sysfsexample_module_exit(void)
 {
-  if (count > WRITE_SIZE) {
-    count = WRITE_SIZE;
+  if (calc_obj) {
+    kobject_put(calc_obj);
+    kfree(calc_obj);
   }
-
-  //memset(arg1_input, 0, WRITE_SIZE);
-  memcpy(arg1_input, buf, count);
-  return count;
 }
 
-/*
-* arg2 write handler
-*/
-int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
-{
-  if (count > WRITE_SIZE) {
-    count = WRITE_SIZE;
-  }
-
-  //memset(arg2_input, 0, WRITE_SIZE);
-  memcpy(arg2_input, buf, count);
-  return count;
-}
-
-/*
-* operation write handler
-*/
-int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
-{
-  if (count > WRITE_SIZE) {
-    count = WRITE_SIZE;
-  }
-
-  //memset(operation, 0, WRITE_SIZE);
-  memcpy(operation_input, buf, count);
-  return count;
-}
-
-/*
-* result read handler
-*/
-int read_result(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data)
-{
-  long res = calculate();
-  return sprintf(buffer, "%ld\n", res);
-}
-
-int init_module()
-{
-  // parent dir
-  calc_dir = proc_mkdir(PARENT_DIR, NULL);
-  if (!calc_dir) {
-    printk(KERN_INFO "Error creating proc entry");
-    return -ENOMEM;
-  }
-
-  // arg1
-  //add virtual files to proc filesystem
-  arg1 = create_proc_entry(ARG1, 0666, calc_dir);
-  if (!arg1) {
-    printk(KERN_INFO "Error creating proc entry");
-    return -ENOMEM;
-  }
-  arg1->write_proc = write_arg1;
-
-  // arg2
-  arg2 = create_proc_entry(ARG2, 0666, calc_dir);
-  if (!arg2) {
-    printk(KERN_INFO "Error creating proc entry");
-    return -ENOMEM;
-  }
-  arg2->write_proc = write_arg2;
-
-  // operation
-  operation = create_proc_entry(OPERATION, 0666, calc_dir);
-  if (!operation) {
-    printk(KERN_INFO "Error creating proc entry");
-    return -ENOMEM;
-  }
-  operation->write_proc = write_operation;
-
-  // result
-  result = create_proc_entry(RESULT, 0666, calc_dir);
-  if (!result) {
-    printk(KERN_INFO "Error creating proc entry");
-    return -ENOMEM;
-  }
-  result->read_proc = read_result;
-
-  printk(KERN_INFO "/proc/%s created\n", PARENT_DIR);
-  return 0;
-}
-
-//delete all virtual files from proc filesystem
-void cleanup_module()
-{
-  remove_proc_entry(ARG1, NULL);
-  remove_proc_entry(ARG2, NULL);
-  remove_proc_entry(OPERATION, NULL);
-  remove_proc_entry(RESULT, NULL);
-  printk(KERN_INFO "/proc/%s removed\n", PARENT_DIR);
-}
-module_init(init_module);
-module_exit(cleanup_module);
+module_init(sysfsexample_module_init);
+module_exit(sysfsexample_module_exit);
 MODULE_LICENSE("GPL");
-#endif
 
